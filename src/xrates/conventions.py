0c692928498bc2e5ba1762edd7188d9e7cf7de4c
Tom Williams <tom.williams@havencove.com>
2023-10-13 14:19:58 +0100

    tests2

--- src/xrates/conventions.py
+++ src/xrates/conventions.py
@@ -4,29 +4,6 @@ from __future__ import annotations
 import enum
 
 import xtuples as xt
-
-# ---------------------------------------------------------------
-
-class Overflow(enum.Enum):
-    ERROR = 0
-    PREV = -1
-    NEXT = 1
-
-# ---------------------------------------------------------------
-
-class Roll(enum.Enum):
-    ERROR = 0
-    PRECEDING = -1
-    FOLLOWING = 1
-
-class Modified(enum.Enum):
-    UNMODIFIED = 0
-    MODIFIED = 1
-
-# ---------------------------------------------------------------
-
-class Format(enum.Enum):
-    ISO = 0
     
 # ---------------------------------------------------------------
 
